affc5ac2afde33af7344c3cdcfb2296bc9f6aa19
Luca Pizzoferrato <mbot@mbot.local>
2022-05-17 18:44:30 -0400

    update

--- 01-Basics/Labs/02-PythonApp/dog-api/main.py
+++ 01-Basics/Labs/02-PythonApp/dog-api/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from typing import Optional
 
 from fastapi import FastAPI
@@ -14,3 +16,5 @@ def read_root():
 def read_dog(dog_id: int, q: Optional[str] = None):
     return {"dog_id": dog_id, "q": q}
 
+def start():
+    uvicorn.run("uvicorn.main:app", host:"127.0.0.1", port=8000, reload=True)
