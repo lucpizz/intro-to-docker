3390ad4f3e524bc47c3fbdab8dbc5c4141e82037
Luca Pizzoferrato <mbot@mbot.local>
2022-05-17 19:16:08 -0400

    updated docker file

--- 01-Basics/Labs/02-PythonApp/dog-api/main.py
+++ 01-Basics/Labs/02-PythonApp/dog-api/main.py
@@ -16,5 +16,3 @@ def read_root():
 def read_dog(dog_id: int, q: Optional[str] = None):
     return {"dog_id": dog_id, "q": q}
 
-def start():
-    uvicorn.run("uvicorn.main:app", host:"127.0.0.1", port=8000, reload=True)
